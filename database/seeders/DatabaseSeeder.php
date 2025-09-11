772faa1681505e65accb63b6aeba81fb16d1528a
Cesar Melo <csrmds@gmail.com>
2025-09-11 00:26:47 -0300

    correção dockerfile para postgresql

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,15 +18,15 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Celia Azevedo',
             'email' => 'celia@email.com',
-            'password' => Hash::make('senha'),
+            'password' => Hash::make('Segredo#ortiz'),
             'avatar'=> 'https://res.cloudinary.com/dufaejhwh/image/upload/v1753755908/avatar_mulher_chanel_jhcvt5.jpg'
         ]);
 
         User::factory()->create([
-            'name' => 'Cris Marins',
-            'email' => 'cris@email.com',
-            'password' => Hash::make('senha'),
-            'avatar'=> 'https://res.cloudinary.com/dufaejhwh/image/upload/v1755651826/avatar_mulher_tranças_agvtpm.jpg'
+            'name' => 'Marcos Marins',
+            'email' => 'marcos@email.com',
+            'password' => Hash::make('Segredo#ortiz'),
+            'avatar'=> 'https://res.cloudinary.com/dufaejhwh/image/upload/v1757283886/avatar_masculino_li8fo1.jpg'
         ]);
     }
 }
--- dockerfile
+++ dockerfile
@@ -2,7 +2,9 @@
 FROM php:8.2-cli AS php-base
 RUN apt-get update && apt-get install -y \
     unzip git curl libpng-dev libonig-dev libxml2-dev zip libzip-dev \
-    && docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd zip
+    libpq-dev \
+    && docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd zip \
+    && docker-php-ext-install pdo_pgsql pgsql
 
 # Instalar Composer
 COPY --from=composer:2.6 /usr/bin/composer /usr/bin/composer
