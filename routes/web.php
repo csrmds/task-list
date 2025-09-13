1781b09b1bb909208d2df796b4fc5c9daf90dd44
Cesar Melo <csrmds@gmail.com>
2025-09-13 17:25:50 -0300

    rotas de teste

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,45 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 Route::get('/auth/google', [App\Http\Controllers\UserController::class, 'authGoogle']);
 Route::get('/auth/callback/google', [App\Http\Controllers\UserController::class, 'authCallback']);
+
+Route::get('/xb', function () {
+	try {
+		\DB::connection()->getPdo();
+
+		$query= User::all();
+
+		
+	return response()->json($query);
+	} catch (\Exception $e) {
+		return 'Erro na conexão: ' . $e->getMessage();
+	}
+});
+
+Route::get('/xa', function () {
+	try {
+		$dbConnection= env('DB_CONNECTION');
+		$dbHost= env('DB_USERNAME');
+		$dbUrl= env('DB_URL');
+		$dbUser= env('DB_USERNAME');
+		$dbDatabase= env('DB_DATABASE');
+		$dbPassword= env('DB_PASSWORD');
+
+		//$query= User::all();
+
+		
+	return response()->json([
+		'Connection'=> $dbConnection,
+		'host'=> $dbHost,
+		'Url'=> $dbUrl,
+		'User'=> $dbUser,
+		'Database'=> $dbDatabase,
+		'pwd'=> $dbPassword,
+	]);
+	} catch (\Exception $e) {
+		return 'Erro na conexão: ' . $e->getMessage();
+	}
+});
