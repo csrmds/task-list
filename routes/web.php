c2c81b791ce62e457f3498d1e25603a30356e942
Cesar Melo <csrmds@gmail.com>
2025-09-13 18:25:33 -0300

    rota xc

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ use App\Models\User;
 Route::get('/auth/google', [App\Http\Controllers\UserController::class, 'authGoogle']);
 Route::get('/auth/callback/google', [App\Http\Controllers\UserController::class, 'authCallback']);
 
+Route::get('/xc', function () {
+	try {
+		return response()->json(teste: "qq informação de retorno");
+	} catch (\Exception $e) {
+		return 'Erro na conexão: ' . $e->getMessage();
+	}
+});
